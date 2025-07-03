ce6fb7dec956c493ffdcc6170ac0f05fd28d1f7c
Erik T. Segura <eriktarruella@gmail.com>
2025-07-03 10:46:20 +0000

    feat(events): adding events definition for player & server

---
+++ src/main/java/com/etarruella/event/ServerEventType.java
@@ -0,0 +1,13 @@
+package com.etarruella.event;
+
+public enum ServerEventType {
+    ServerStartEvent,
+    ServerStopEvent,
+    ServerReloadEvent,
+    ServerLoadCompleteEvent,
+    ServerTickEvent,
+    ServerCommandEvent,
+    ServerBroadcastMessageEvent,
+    ServerSaveEvent,
+    ServerPlayerCountChangeEvent
+}
