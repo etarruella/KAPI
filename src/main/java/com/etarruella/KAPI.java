370597b51028fe77333ba43567c29de93c86a1e3
Erik T. Segura <eriktarruella@gmail.com>
2025-07-08 09:41:06 +0000

    feat(main): load main config and web & game socket managers

--- src/main/java/com/etarruella/KAPI.java
+++ src/main/java/com/etarruella/KAPI.java
@@ -9,11 +9,30 @@ import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityRegainHealthEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import config.ConfigManager;
+import network.GameEventWebSocketServer;
+import network.WebSocketServerManager;
+
 public class KAPI extends JavaPlugin {
+
+    private ConfigManager configManager;
+    private WebSocketServerManager webSocketServerManager;
+    private GameEventWebSocketServer gameEventWebSocketServer;
 
     @Override
     public void onEnable() {
-        Bukkit.getPluginManager().registerEvents(this, this);
+        // Load config
+        configManager = new ConfigManager(this);
+
+        // Load WebSocketServer
+        webSocketServerManager = new WebSocketServerManager(
+                configManager.getNetworkHost,
+                configManager.getNetworkPort());
+
+        // Load GameEventWebSocketServer
+        gameEventWebSocketServer = new GameEventWebSocketServer(
+                configManager.getNetworkHost(),
+                webSocketServerManager);
     }
 
 }
