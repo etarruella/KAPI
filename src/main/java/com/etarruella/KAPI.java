5a90a8ec2168a0c5322879404ef20fc8af4ead3c
Erik T. Segura <eriktarruella@gmail.com>
2025-07-08 08:23:17 +0000

    fix(main): deleted old test listeners

--- src/main/java/com/etarruella/KAPI.java
+++ src/main/java/com/etarruella/KAPI.java
@@ -10,27 +10,10 @@ import org.bukkit.event.entity.EntityRegainHealthEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class KAPI extends JavaPlugin implements Listener {
+
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
-    }
-
-    @EventHandler
-    public void onEntityDamageEvent(EntityDamageEvent event) {
-        if (event.getEntityType() == EntityType.PLAYER) {
-            LivingEntity entity = (LivingEntity) event.getEntity();
-            double newLife = Math.max(entity.getHealth() - event.getDamage(), 0);
-            entity.sendMessage("Vida -: " + newLife);
-        }
-    }
-
-    @EventHandler
-    public void onEntityRegainHealthEvent(EntityRegainHealthEvent event) {
-        if (event.getEntityType() == EntityType.PLAYER) {
-            LivingEntity entity = (LivingEntity) event.getEntity();
-            double newLife = Math.min(entity.getHealth() + event.getAmount(), entity.getMaxHealth());
-            entity.sendMessage("Vida +: " + newLife);
-        }
     }
 
 }
