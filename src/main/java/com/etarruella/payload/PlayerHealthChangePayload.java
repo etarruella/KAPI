262c0eb0443b31d382a0d000ceaa622c793af572
Erik T. Segura <eriktarruella@gmail.com>
2025-07-14 06:54:46 +0000

    feat: adding oldHealth value to payload

--- src/main/java/com/etarruella/payload/PlayerHealthChangePayload.java
+++ src/main/java/com/etarruella/payload/PlayerHealthChangePayload.java
@@ -1,21 +1,27 @@
 package com.etarruella.payload;
 
 public class PlayerHealthChangePayload {
+
     private String event;
     private String playerUUID;
     private double newHealth;
+    private double oldHealth;
 
-    public PlayerHealthChangePayload(String event, String playerUUID, double newHealth) {
+    public PlayerHealthChangePayload(String event, String playerUUID, double oldHealth, double newHealth) {
         this.event = event;
         this.playerUUID = playerUUID;
+        this.oldHealth = oldHealth;
         this.newHealth = newHealth;
     }
 
     public String getEvent() { return event; }
     public String getPlayerUUID() { return playerUUID; }
+    public double getOldHealth() { return oldHealth; }
     public double getNewHealth() { return newHealth; }
 
     public void setEvent(String event) { this.event = event; }
     public void setPlayerUUID(String playerUUID) { this.playerUUID = playerUUID; }
+    public void setOldHealth(double oldHealth) { this.oldHealth = oldHealth; }
     public void setNewHealth(double newHealth) { this.newHealth = newHealth; }
+
 }
