45335991459832fcf0511ef7e53ab135d07eef8c
Erik Tarruella <eriktarruella@gmail.com>
2025-07-14 18:21:36 +0200

    fix: type string --> UUID

--- src/main/java/com/etarruella/payload/PlayerHealthChangePayload.java
+++ src/main/java/com/etarruella/payload/PlayerHealthChangePayload.java
@@ -1,13 +1,15 @@
 package com.etarruella.payload;
 
+import java.util.UUID;
+
 public class PlayerHealthChangePayload {
 
     private String event;
-    private String playerUUID;
+    private UUID playerUUID;
     private double newHealth;
     private double oldHealth;
 
-    public PlayerHealthChangePayload(String event, String playerUUID, double oldHealth, double newHealth) {
+    public PlayerHealthChangePayload(String event, UUID playerUUID, double oldHealth, double newHealth) {
         this.event = event;
         this.playerUUID = playerUUID;
         this.oldHealth = oldHealth;
@@ -15,12 +17,12 @@ public class PlayerHealthChangePayload {
     }
 
     public String getEvent() { return event; }
-    public String getPlayerUUID() { return playerUUID; }
+    public String getPlayerUUID() { return playerUUID.toString(); }
     public double getOldHealth() { return oldHealth; }
     public double getNewHealth() { return newHealth; }
 
     public void setEvent(String event) { this.event = event; }
-    public void setPlayerUUID(String playerUUID) { this.playerUUID = playerUUID; }
+    public void setPlayerUUID(UUID playerUUID) { this.playerUUID = playerUUID; }
     public void setOldHealth(double oldHealth) { this.oldHealth = oldHealth; }
     public void setNewHealth(double newHealth) { this.newHealth = newHealth; }
 
